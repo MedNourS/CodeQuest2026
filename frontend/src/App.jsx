dcb70363167ef20240c956eda5830827847d478d
Mikael L <mikaelharvard@gmail.com>
2026-02-28 13:38:38 -0500

    fixed merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,9 @@ import { useState } from 'react'
 import MiNYLogo from '../public/logo.svg'
 import './App.css'
 import { distance } from 'fastest-levenshtein'
-<<<<<<< HEAD
-import FoodItem from "./components/FoodSection"
-import FoodSection from './components/FoodSection'
 import HorizontalLogo from "./assets/Horizontal_Logo.png"
 import SelectedIngredients from './components/SelectedIngredients'
-=======
 import IngredientSection from './components/IngredientSection'
->>>>>>> b5ca3433fbef46324cfec3706ea8dcd4750c9c6c
 
 function App() {
   return (

f0342ce1e25b464c06146a58106f87e212828404
Yaroslav K <yarokl100@gmail.com>
2026-02-28 16:44:33 -0500

    i dunno

--- frontend/src/components/IngredientItem.jsx
+++ frontend/src/components/IngredientItem.jsx
@@ -1,16 +1,24 @@
 import './IngredientItem.css'
+import { useState } from 'react';
 
 function IngredientItem(props) {
+  const [render, setRender] = useState(0);
+
   const addToLS = () => {
-    const ingredientList = localStorage.getItem('ingredients');
+    const ingredientList = JSON.parse(localStorage.getItem('ingredients'));
+    if (ingredientList == null){
+      localStorage.setItem('ingredients', JSON.stringify([props.name]));
+      ingredientList = [props.name];
+    }
     if(ingredientList.includes(props.name)) return;
     localStorage.setItem('ingredients', JSON.stringify([...ingredientList, props.name]));
+    setRender(render + 1);
   }
 
   return (
     <div className="ingredientItem">
-      <p className="ingredientName">{name}</p>
-      <button className="addBtn" >+</button>
+      <p className="ingredientName">{props.name}</p>
+      <button className="addBtn" onClick={addToLS}>+</button>
     </div>
   )
 }

1a9f8c23ba796337430965b65e4355c216fd1bd5
Mikael L <mikaelharvard@gmail.com>
2026-02-28 17:54:54 -0500

    css fixes and media query

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,33 @@
 import { useState } from 'react'
-import MiNYLogo from '../public/logo.svg'
 import './App.css'
-import { distance } from 'fastest-levenshtein'
 import HorizontalLogo from "./assets/Horizontal_Logo.png"
 import SelectedIngredients from './components/SelectedIngredients'
 import IngredientSection from './components/IngredientSection'
-import Utility from './components/Utilitiy'
-
 
 function App() {
   const [selectedIngredients, setSelectedIngredients] = useState(JSON.parse(localStorage.getItem("ingredients")))
 
   return (
-    <>
-      <div className="layout">
-      <head>
-        <title>MiNY Crave</title>
-      </head>
-        <header className="header">
-          <div className="headerLeft"></div>
-              <img src={HorizontalLogo} alt="Horizontal Logo" className="logo" />
-          <div className="headerRight">
-            <button className="headerBtn">Get Recipes!</button>
+    <div className="layout">
+      <div className="header">
+        <div className="headerLeft">
+          <img src={HorizontalLogo} alt="Horizontal Logo" className="logo" />
+        </div>
+        <div className="headerRight">
+          <button className="headerBtn">Get Recipes!</button>
+        </div>
       </div>
-        </header>
-
-        <div className="body">
-          <div className="ingredientSection"><IngredientSection setSelectedIngredients={setSelectedIngredients}/></div>
-          <div className="rightPanel">
-            <div className="selectedIngredients"><SelectedIngredients setSelectedIngredients={setSelectedIngredients}/></div>
-            <div className="utility"><Utility /> </div>
+      <div className="body">
+        <div className="ingredientSection">
+          <IngredientSection setSelectedIngredients={setSelectedIngredients}/>
+        </div>
+        <div className="rightPanel">
+          <div className="selectedIngredients">
+            <SelectedIngredients setSelectedIngredients={setSelectedIngredients}/>
           </div>
         </div>
-
       </div>
-    </>
+    </div>
   )
 }
 
